53037d210844f598fb083c32fc865303515ea8f4
gj-asai <63119784+gj-asai@users.noreply.github.com>
2023-06-08 11:12:42 +0200

    Add files via upload

---
+++ filters.py
@@ -0,0 +1,90 @@
+import numpy as np
+from scipy.sparse import coo_matrix
+from scipy.ndimage import gaussian_filter
+
+# https://www.rmit.edu.au/research/centres-collaborations/centre-for-innovative-structures-and-materials/software
+"""
+Weights: max{0, rmin-distance(i,j)}
+"""
+class ConvolutionFilter():
+    def __init__(self, rmin, num_elem, centers):
+        self.H = ConvolutionFilter.preFlt(rmin, num_elem, centers)
+        
+    def distances(matrixA, matrixB):
+        A = np.matrix(matrixA)
+        B = np.matrix(matrixB)
+        Btrans = B.transpose()
+        vecProd = A * Btrans
+        SqA =  A.getA()**2
+        sumSqA = np.matrix(np.sum(SqA, axis=1))
+        sumSqAEx = np.tile(sumSqA.transpose(), (1, vecProd.shape[1]))    
+        SqB = B.getA()**2
+        sumSqB = np.sum(SqB, axis=1)
+        sumSqBEx = np.tile(sumSqB, (vecProd.shape[0], 1))    
+        SqED = sumSqBEx + sumSqAEx - 2*vecProd   
+        elmDis = (np.maximum(0,SqED).getA())**0.5
+        return np.matrix(elmDis)
+    
+    def preFlt(rmin, num_elem, centers):
+        if rmin == 0: return np.identity(num_elem)
+        try:
+            limitElementNumber = 4000 # should be larger than (2*(rmin/elmsize)) ** 2
+            nfilter=int(num_elem*limitElementNumber) 
+            iH,jH,sH,cc = np.zeros(nfilter),np.zeros(nfilter),np.zeros(nfilter),0
+            for ei in range(num_elem):
+                ii = np.where(abs(centers[:,0] - centers[ei][0]) < rmin)[0]
+                jj = np.where(abs(centers[ii,1] - centers[ei][1]) < rmin)[0]
+                kk = np.where(abs(centers[ii[jj],2] - centers[ei][2]) < rmin)[0]
+                neighbors = ii[jj][kk]
+                iH[cc:cc+len(neighbors)] = ei
+                jH[cc:cc+len(neighbors)] = neighbors
+                eiH = np.maximum(0,rmin - ConvolutionFilter.distances(centers[ei],centers[neighbors]))
+                sH[cc:cc+len(neighbors)] = eiH / np.sum(eiH)
+                cc += len(neighbors)
+            H = coo_matrix((sH,(iH,jH)),shape=(num_elem,num_elem))
+        except:
+            H = np.identity(num_elem)
+            print('\n***   Insufficient memory or small limitElementNumber    ***\n')
+        return H
+    
+class MeshIndependencyFilter(ConvolutionFilter):
+    def __init__(self, rmin, num_elem, centers):
+        super().__init__(rmin,num_elem,centers)
+        
+    def filter(self, x, dc):
+        return self.H.dot(x*dc)/x
+
+class OrientationRegularizationFilter(ConvolutionFilter):
+    def __init__(self, rmin, num_elem, centers):
+        super().__init__(rmin,num_elem,centers)
+        self.H = self.H.tocsr()
+        
+    def filter(self, theta):
+        sign = np.sign(np.outer(theta,theta))
+        sign[np.where(sign==0)] = -1
+        return self.H.multiply(sign).dot(theta)
+    
+class GaussianFilter():   
+    def __init__(self, num_elem, centers):
+        self.num_elem = num_elem
+        self.centers = centers
+        self.x, self.y = np.meshgrid(np.unique(centers[:,0]),np.unique(centers[:,1]))
+        
+        # Matrix i,j correspondence for each element
+        self.e = range(num_elem)
+        self.i = []
+        self.j = []
+        for e in self.e:
+            self.i.append(np.where(self.x[0,:] == self.centers[e,0])[0][0])
+            self.j.append(np.where(self.y[:,0] == self.centers[e,1])[0][0])
+        
+    def filter(self, theta):
+        theta_grid = np.zeros_like(self.x)
+        theta_grid[self.j,self.i] = theta[self.e]
+        
+        theta_grid = gaussian_filter(theta_grid, sigma=0.5)
+        
+        thetanew = np.zeros_like(theta)
+        thetanew[self.e] = theta_grid[self.j,self.i]
+            
+        return thetanew
---
+++ postprocessor.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from functools import partial
+
+from optimization import TopOpt
+
+class PostProcessor():
+    def __init__(self, solver):
+        self.solver = solver
+        
+    def make_grid(self, result):
+        x, y = np.meshgrid(np.unique(self.solver.centers[:,0]),np.unique(self.solver.centers[:,1]))
+        z = np.zeros_like(x)
+        for e in range(self.solver.num_elem):
+            i = np.where(x[0,:] == self.solver.centers[e,0])[0][0]
+            j = np.where(y[:,0] == self.solver.centers[e,1])[0][0]
+            z[j,i] = result[e]
+            
+        return x, y, z
+        
+    def plot(self, iteration=-1, fig=None, ax=None):
+        if fig == None: fig, ax = plt.subplots(dpi=300)
+        ax.cla()
+        ax.set_aspect('equal', 'box')
+        plt.title('Compliance = {:.4f}'.format(self.solver.comp_hist[iteration]))
+        
+        x, y, density = self.make_grid(self.solver.rho_hist[iteration])
+        ax.pcolormesh(x, y, density, cmap='binary')
+        
+        _, _, theta = self.make_grid(self.solver.theta_hist[iteration])
+        ax.quiver(x, y, np.cos(theta), np.sin(theta), color='white', pivot='mid', headwidth=0, headlength=0, headaxislength=0)
+        
+    def plot_orientation(self, iteration=-1, fig=None, ax=None):
+        if fig == None: fig, ax = plt.subplots(dpi=300)
+        ax.cla()
+        ax.set_aspect('equal', 'box')
+        plt.title('Compliance = {:.4f}'.format(self.solver.comp_hist[iteration])) 
+        
+        x, y, theta = self.make_grid(self.solver.theta_hist[iteration])
+        ax.quiver(x, y, np.cos(theta), np.sin(theta), pivot='mid', headwidth=0, headlength=0, headaxislength=0)
+       
+    def plot_convergence(self, compliance_unit='N.mm'):
+        plt.figure()
+        plt.plot(self.solver.comp_hist)
+        plt.xlabel('Iteration')
+        plt.ylabel(f'Compliance [{compliance_unit}]')
+        
+    def animate(self, filename=None):
+        if filename is None: filename = TopOpt.res_dir + 'animation.gif'
+        fig, ax = plt.subplots(dpi=300)
+        anim = FuncAnimation(fig, partial(self.plot, fig=fig, ax=ax), frames=len(self.solver.rho_hist))
+        anim.save(filename)
+        
+    def animate_orientation(self, filename=None):
+        if filename is None: filename = TopOpt.res_dir + 'animation_o.gif'
+        fig, ax = plt.subplots(dpi=300)
+        anim = FuncAnimation(fig, partial(self.plot_orientation, fig=fig, ax=ax), frames=len(self.solver.rho_hist))
+        anim.save(filename)
+        
+    def CO2_footprint(self, rho, CO2mat, CO2veh):
+        """
+        rho: density
+        CO2mat: mass CO2 emmited per mass material (material production)
+        CO2veh: mass CO2 emitted per mass material during life (use in a vehicle)
+                = mass fuel per mass transported per lifetime * service life * mass CO2 emmited per mass fuel
+        """
+        x = self.solver.rho_hist[-1]
+        mass = rho * x.dot(self.solver.elemvol)
+        return mass * (CO2mat + CO2veh)
