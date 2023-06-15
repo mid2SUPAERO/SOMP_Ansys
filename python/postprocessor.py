b2eb514689a7afe50f249563815bc3a175166cfa
gj-asai <gustavojun.asai@gmail.com>
2023-06-15 09:22:11 +0200

    Scratch of 3D plot

--- python/postprocessor.py
+++ python/postprocessor.py
@@ -71,3 +71,65 @@ class PostProcessor():
         x = self.solver.rho_hist[-1]
         mass = rho * x.dot(self.solver.elemvol)
         return mass * (CO2mat + CO2veh)
+
+class Post3D():
+    def __init__(self, solver):
+        self.solver = solver
+
+    # https://stackoverflow.com/questions/40853556/3d-discrete-heatmap-in-matplotlib
+    def cuboid_data(center, size=1):
+        # code taken from
+        # http://stackoverflow.com/questions/30715083/python-plotting-a-wireframe-3d-cuboid?noredirect=1&lq=1
+        # suppose axis direction: x: to left; y: to inside; z: to upper
+        # get the (left, outside, bottom) point
+        o = [a - b / 2 for a, b in zip(center, size)]
+        # get the length, width, and height
+        l = w = h = size
+        x = [[o[0], o[0] + l, o[0] + l, o[0], o[0]],  # x coordinate of points in bottom surface
+             [o[0], o[0] + l, o[0] + l, o[0], o[0]],  # x coordinate of points in upper surface
+            [o[0], o[0] + l, o[0] + l, o[0], o[0]],  # x coordinate of points in outside surface
+            [o[0], o[0] + l, o[0] + l, o[0], o[0]]]  # x coordinate of points in inside surface
+        y = [[o[1], o[1], o[1] + w, o[1] + w, o[1]],  # y coordinate of points in bottom surface
+             [o[1], o[1], o[1] + w, o[1] + w, o[1]],  # y coordinate of points in upper surface
+             [o[1], o[1], o[1], o[1], o[1]],          # y coordinate of points in outside surface
+             [o[1] + w, o[1] + w, o[1] + w, o[1] + w, o[1] + w]]    # y coordinate of points in inside surface
+        z = [[o[2], o[2], o[2], o[2], o[2]],                        # z coordinate of points in bottom surface
+             [o[2] + h, o[2] + h, o[2] + h, o[2] + h, o[2] + h],    # z coordinate of points in upper surface
+             [o[2], o[2], o[2] + h, o[2] + h, o[2]],                # z coordinate of points in outside surface
+             [o[2], o[2], o[2] + h, o[2] + h, o[2]]]                # z coordinate of points in inside surface
+        return x, y, z
+
+    def plotCubeAt(pos=(0,0,0), c="b", alpha=0.1, ax=None):
+        # Plotting N cube elements at position pos
+        if ax !=None:
+            X, Y, Z = cuboid_data( (pos[0],pos[1],pos[2]) )
+            ax.plot_surface(X, Y, Z, color=c, rstride=1, cstride=1, alpha=0.1)
+    
+    def plotMatrix(ax, x, y, z, data, cax=None, alpha=0.1):
+        # plot a Matrix 
+        norm = matplotlib.colors.Normalize(vmin=0., vmax=1.)
+        colors = lambda i : matplotlib.cm.ScalarMappable(norm=norm,cmap = 'binary').to_rgba(data[i]) 
+        for i in range(len(x)):
+            plotCubeAt(pos=(x[i],y[i],z[i]), c=colors(i), alpha=alpha, ax=ax)
+
+        if cax != None:
+            cbar = matplotlib.colorbar.ColorbarBase(cax, cmap='binary', norm=norm, orientation='vertical')  
+            cbar.set_ticks(np.unique(data))
+            # set the colorbar transparent as well
+            cbar.solids.set(alpha=alpha)     
+    
+    def plot(self, iteration=-1, filename=None, save=True):
+        x = self.solver.centers[:,0]
+        y = self.solver.centers[:,1]
+        z = self.solver.centers[:,2]
+        data = self.solver.rho_hist[iteration]
+
+        fig = plt.figure(figsize=(10,4))
+        ax = fig.add_axes([0.1, 0.1, 0.7, 0.8], projection='3d')
+        ax_cb = fig.add_axes([0.8, 0.3, 0.05, 0.45])
+        ax.set_aspect('equal')
+    
+        plotMatrix(ax, x, y, z, data, cax=ax_cb)
+        if save:
+            if filename is None: filename = self.solver.res_dir / 'design.png'
+            plt.savefig(filename)
