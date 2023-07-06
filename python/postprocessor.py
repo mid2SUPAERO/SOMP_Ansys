8537593eea4c0d07432ebdaa0fab7e214f7f5bdb
gj-asai <63119784+gj-asai@users.noreply.github.com>
2023-07-06 22:50:46 +0200

    Update postprocessor.py

--- python/postprocessor.py
+++ python/postprocessor.py
@@ -93,7 +93,6 @@ class Post3D(PostProcessor):
         plt.ylim(np.amin(self.solver.node_coord[:,1]),np.amax(self.solver.node_coord[:,1]))
         plt.title('Layer {} - Compliance = {:.4f}'.format(layer, self.solver.comp_hist[iteration]))
         
-<<<<<<< HEAD
         z = np.unique(self.solver.centers[:,2])[layer]
         idx = np.where(self.solver.centers[:,2] == z)[0]
         
@@ -110,53 +109,10 @@ class Post3D(PostProcessor):
             color = 'black'
         
         ax.quiver(x, y, u, v, color=color, alpha=rho, pivot='mid', headwidth=0, headlength=0, headaxislength=0, linewidth=1.5)
-=======
-        x, y, density = self.make_grid(layer, self.solver.rho_hist[iteration], nodes=True)
-        norm = colors.Normalize(vmin=0, vmax=1)
-        ax.pcolormesh(x, y, density, cmap='binary', norm=norm)
-        
-        x, y, theta = self.make_grid(layer, self.solver.theta_hist[iteration], nodes=False)
-        ax.quiver(x, y, np.cos(theta), np.sin(theta), color='white', pivot='mid', headwidth=0, headlength=0, headaxislength=0)
->>>>>>> b9e2e13110c51d37cde6f045b8067f9343d76e8f
 
         if save:
             if filename is None: filename = self.solver.res_dir / f'design_layer{layer}.png'
             plt.savefig(filename)
-<<<<<<< HEAD
-=======
-            
-    def plot_orientation(self, iteration=-1, threshold=0.3, elev=None, azim=None, filename=None, save=True, fig=None, ax=None):
-        if ax is None:
-            fig = plt.figure(dpi=500)
-            ax = fig.add_axes([0,0,1,1], projection='3d')
-            deltax = np.amax(self.solver.node_coord[:,0]) - np.amin(self.solver.node_coord[:,0])
-            deltay = np.amax(self.solver.node_coord[:,1]) - np.amin(self.solver.node_coord[:,1])
-            deltaz = np.amax(self.solver.node_coord[:,2]) - np.amin(self.solver.node_coord[:,2])
-            ax.set_box_aspect((deltax,deltay,deltaz))            
-            ax.view_init(elev=elev, azim=azim)
-        ax.cla()
-        plt.title('Compliance = {:.4f}'.format(self.solver.comp_hist[iteration])) 
-        
-        x = self.solver.centers[:,0]
-        y = self.solver.centers[:,1]
-        z = self.solver.centers[:,2]
-        theta = self.solver.theta_hist[iteration]
-        alpha = self.solver.alpha_hist[iteration]
-        u = np.cos(alpha)*np.cos(theta)
-        v = np.cos(alpha)*np.sin(theta)
-        w = np.sin(alpha)
-        
-        rho = self.solver.rho_hist[iteration]
-        x = x[rho > threshold]
-        y = y[rho > threshold]
-        z = z[rho > threshold]
-        u = u[rho > threshold]
-        v = v[rho > threshold]
-        w = w[rho > threshold]
-        color = [(u[i],v[i],w[i]) for i in range(len(u))]
-        
-        ax.quiver(x, y, z, u, v, w, color=color, pivot='middle', arrow_length_ratio=0, linewidth=0.3, length=3)
->>>>>>> b9e2e13110c51d37cde6f045b8067f9343d76e8f
         
     def animate(self, filename=None, colorful=True, elev=None, azim=None):
         if filename is None: filename = self.solver.res_dir / 'animation.gif'
@@ -169,55 +125,5 @@ class Post3D(PostProcessor):
     def animate_layer(self, layer, colorful=False, filename=None):
         if filename is None: filename = self.solver.res_dir / f'animation_layer{layer}.gif'
         fig, ax = plt.subplots(dpi=300)
-<<<<<<< HEAD
         anim = FuncAnimation(fig, partial(self.plot_layer, layer=layer, colorful=colorful, save=False, fig=fig, ax=ax), frames=len(self.solver.rho_hist))
         anim.save(filename)
-=======
-        anim = FuncAnimation(fig, partial(self.plot_layer, layer=layer, save=False, fig=fig, ax=ax), frames=len(self.solver.rho_hist))
-        anim.save(filename)
-    
-    ## --------------
-    def plotCube(self, ax, elem, c, alpha=0.5):
-        nodes = self.solver.elmnodes[elem,:]
-        coord = self.solver.node_coord[nodes,:]
-        
-        # bottom z-
-        x, y = np.meshgrid(coord[[0,2],0], coord[[0,2],1])
-        z = np.array([[coord[0,2],coord[1,2]],[coord[3,2],coord[2,2]]])
-        ax.plot_surface(x, y, z, color=c, rstride=1, cstride=1, alpha=alpha)
-        # upper z+
-        x, y = np.meshgrid(coord[[4,6],0], coord[[4,6],1])
-        z = np.array([[coord[4,2],coord[5,2]],[coord[7,2],coord[6,2]]])
-        ax.plot_surface(x, y, z, color=c, rstride=1, cstride=1, alpha=alpha)
-        # left x-
-        x, y = np.meshgrid(coord[[2,7],0], coord[[2,7],1])
-        z = np.array([[coord[2,2],coord[3,2]],[coord[6,2],coord[7,2]]])
-        ax.plot_surface(x, y, z, color=c, rstride=1, cstride=1, alpha=alpha)
-        # right x+
-        x, y = np.meshgrid(coord[[0,5],0], coord[[0,5],1])
-        z = np.array([[coord[0,2],coord[1,2]],[coord[4,2],coord[5,2]]])
-        ax.plot_surface(x, y, z, color=c, rstride=1, cstride=1, alpha=alpha)
-        # front y+
-        x, y = np.meshgrid(coord[[1,6],0], coord[[1,6],1])
-        z = np.array([[coord[1,2],coord[5,2]],[coord[2,2],coord[6,2]]])
-        ax.plot_surface(x, y, z, color=c, rstride=1, cstride=1, alpha=alpha)
-        # back y-
-        x, y = np.meshgrid(coord[[0,7],0], coord[[0,7],1])
-        z = np.array([[coord[0,2],coord[4,2]],[coord[3,2],coord[7,2]]])
-        ax.plot_surface(x, y, z, color=c, rstride=1, cstride=1, alpha=alpha)
-        
-    def make_grid(self, layer, result, nodes):
-        x, y = np.meshgrid(np.unique(self.solver.centers[:,0]),np.unique(self.solver.centers[:,1]))
-        z = np.unique(self.solver.centers[:,2])[layer]
-        res = np.zeros_like(x)
-        for e in range(self.solver.num_elem):
-            if not self.solver.centers[e,2] == z: continue
-            i = np.where(x[0,:] == self.solver.centers[e,0])[0][0]
-            j = np.where(y[:,0] == self.solver.centers[e,1])[0][0]
-            res[j,i] = result[e]
-        
-        if nodes:
-            x, y = np.meshgrid(np.unique(self.solver.node_coord[:,0]),np.unique(self.solver.node_coord[:,1]))
-            
-        return x, y, res
->>>>>>> b9e2e13110c51d37cde6f045b8067f9343d76e8f
